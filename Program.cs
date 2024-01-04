2a11e9fd7c18ed0ee3a0d6c434008570054f941d
Esdras <esdrasbalves10@gmail.com>
2024-01-04 18:47:15 -0300

    conclusão do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("40028922", "Modelo Tijolo", "42123659", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iphone = new Iphone("48903610", "Xs", "51566365", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
